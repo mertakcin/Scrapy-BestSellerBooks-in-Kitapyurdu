cb86753fe5be7397a9c9b9faebb9ba4508bf7f2e
Mertakcin <33595845+mertakcin@users.noreply.github.com>
2020-05-18 19:34:30 +0300

    Second Update

--- kitapyurdu/kitapyurdu/spiders/book_spider.py
+++ kitapyurdu/kitapyurdu/spiders/book_spider.py
@@ -10,6 +10,8 @@ class BooksSpider(scrapy.Spider):
         "https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=1&filter_in_stock=1&filter_in_stock=1&page=1"
     ]
 
+# In this part of code, the code block gets the information of each book like name of the book, author of the book and publisher of the book.    
+    
     def parse(self, response):
         book_names = response.css("div.name.ellipsis a span::text").extract()
         book_authors = response.css("div.author.compact.ellipsis a::text").extract()
@@ -28,11 +30,13 @@ class BooksSpider(scrapy.Spider):
             self.book_count += 1
             
             i += 1
+            
+# In this part of code, we are saying that if there is a next_url, just go there. If not, just finish the process
 
         next_url = response.css("a.next::attr(href)").extract_first()
         self.page_count += 1
 
-        if next_url is not None and self.page_count != 3:
+        if next_url is not None and self.page_count != 3: # In this case, We said that take the book which are under first 3 pages. You can change the value as you wish from 3 to 10 or something.
 
             yield scrapy.Request(url = next_url, callback = self.parse)
 
